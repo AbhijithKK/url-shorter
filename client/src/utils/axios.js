5feb64b48e9dc7310ed6d40b96b8eb2a27d5fcf8
white <abhijithkkpurameri@gmail.com>
2023-09-20 12:26:58 +0530

    test

--- client/src/utils/axios.js
+++
@@ -1,8 +0,0 @@
-import Axios from 'axios'
-
-
-export const Axioss = Axios.create({
-    baseURL: 'http://localhost:4000',
-    headers: {'Content-Type':'application/json'},
-    withCredentials:true
-  });
